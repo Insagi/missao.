6b5d061f07e254fb2c31e2b2df1db635a1c0f228
Insagi <128197337+Insagi@users.noreply.github.com>
2024-10-18 22:31:22 -0300

    s

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,8 @@
+const nomes = ["Dyulia", "Vagner", "Tim Burton"];
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
     return lista[posicao];
 }
+
+
+export const nome = aleatorio(nomes);
